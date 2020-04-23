3629ea37bbe437f0e7b607c6738985e0acce493c
Ashraf <sm.ashraf.zaman>
2020-04-23 15:04:40 +0600

    Print 1 to 10 using for loop in C program

---
+++ time-table.c
@@ -0,0 +1,26 @@
+/*
+    Enter any number : 5
+
+    5 x 1 = 5
+    5 x 2 = 10
+    5 x 3 = 15
+    ..........
+    5 x 10 = 50
+*/
+#include <stdio.h>
+int main()
+{
+        int num, i, result;
+
+        printf("Enter any number : ");
+        scanf("%d",&num);
+
+        for(i=1; i<=10; i++)
+        {
+            result = num * i;
+            printf("%d x %d = %d\n",num, i, result);
+        }
+
+    getch();
+    return 0;
+}
